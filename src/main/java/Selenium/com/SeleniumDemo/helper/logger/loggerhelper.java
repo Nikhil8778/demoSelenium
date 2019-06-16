b52ffcc75dcdbf903be52a0ca204dd93e07c6913
Preeti Gupta <nikhilgupta.4488@gmail.com>
2019-06-16 10:38:05 -0400

    First Project

---
+++ src/main/java/Selenium/com/SeleniumDemo/helper/logger/loggerhelper.java
@@ -0,0 +1,10 @@
+package Selenium.com.SeleniumDemo.helper.logger;
+
+public class loggerhelper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/Selenium/com/SeleniumDemo/helper/resource/resourceHelper.java
@@ -0,0 +1,10 @@
+package Selenium.com.SeleniumDemo.helper.resource;
+
+public class resourceHelper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
